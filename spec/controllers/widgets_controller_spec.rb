6a4293290bfaab671ad1e24a34adf9bf357139a3
SRozick <steven.rozick@teachafish.org>
2014-10-11 13:58:25 -0400

    Refactor widgets_controller_spec.rb

--- spec/controllers/widgets_controller_spec.rb
+++ spec/controllers/widgets_controller_spec.rb
@@ -161,22 +161,24 @@ RSpec.describe WidgetsController do
   #########################################################
 describe 'PATCH update' do
   # see http://stackoverflow.com/a/24739399/3780876
+  let(:widget)                      { create :widget }
   let (:setup)                      {
-                                      create :widget
-                                      @attributes_before_edit = Widget.new
+                                      widget
+                                      @before_edit = Widget.new
                                       widget.attributes.each_pair do |key, value|
-                                        @attributes_before_edit[key] = value
+                                        @before_edit[key] = value
                                       end
                                     }
-  let(:widget)                      { create :widget }
-
-  let(:attributes_changed_in_form)  { { "name" => 'Great updated product' } }
+  let(:attributes_updated_on_form)  { { "name" => 'Great updated product' } }
   let(:attributes_changed_elsewhere){ { "picture" => nil } } #include any attributes changed by controller
-  let(:expected_final_attributes)   { attributes_changed_in_form.merge(attributes_changed_elsewhere) }
-  let(:action)                      { patch :update, id: widget.id, widget: attributes_changed_in_form }
+  let(:expected_final_attributes)   { attributes_updated_on_form.merge(attributes_changed_elsewhere) }
+  let(:action)                      { patch :update, id: widget.id, widget: attributes_updated_on_form }
   let(:for_the_same_widget)         { expect(widget.reload.id).to eq(widget.id) }
   let(:invalid_update_attributes)   { { name: "" } }
   let(:invalid_action)              { patch :update, id: widget.id, widget: invalid_update_attributes }
+  #TODO: Refactor.  Test should not contain expect within let block.  Expect should be within the "It... does" block.
+  #TODO: Refactor.  Test should be much easier to read.
+  #TODO: The goal of these two tests is to generically validate the patch/put controller action for any controller or set of changed attributes.  Why?  To ensure that if any code changes which unexpectedly changes the model or model object, a test negative results.  <I understand it is not normal practice to test controllers, but I have yet to see a valid explanation.  if TDD and BDD works to develop Ruby, then controllers must be tested too.  And I have seen applications that have grown beyond their acceptable maintenance capacity because they don't have adequate testing at a basic level.>
   let(:expect_reload_to_eq_final)   { expected_final_attributes.each_pair do |ukey, uvalue|
                                         widget.attributes.each_pair do |key, value|
                                           if key == ukey
@@ -186,7 +188,7 @@ describe 'PATCH update' do
                                       end #each expected_final
                                     }
   let(:expect_unchanged_to_be_same) {
-                                      @attributes_before_edit.attributes.each_pair do |key, value|
+                                      @before_edit.attributes.each_pair do |key, value|
                                         unchanged = true
                                         expected_final_attributes.each_pair do |ukey, uvalue|
                                           # find out if value was changed during update action.
@@ -196,10 +198,10 @@ describe 'PATCH update' do
                                         end #expected_final loop
                                         # if value wasn't changed, test against before and after update values
                                         if unchanged == true
-                                          if @attributes_before_edit[key].is_a? ActiveSupport::TimeWithZone
+                                          if @before_edit[key].is_a? ActiveSupport::TimeWithZone
                                             expect(widget[:created_at]).to be_within(0.00001).of(widget.reload[:created_at])
                                           else
-                                            expect(@attributes_before_edit[key]).to eq(widget.reload[key])
+                                            expect(@before_edit[key]).to eq(widget.reload[key])
                                           end #test TimeWithZone
                                         end #if
                                       end #@attributes loop
@@ -209,24 +211,23 @@ describe 'PATCH update' do
 
     before { setup }
 
-    it "updates the widget with changed values" do
-      action
-      expect_reload_to_eq_final
-    end #case updates widget
+      it "redirects to the widget :show template" do
+        expect(action).to redirect_to(widget)
+      end #case redirect to :show
 
-    it "does not change unchanged values" do
-      action
-      expect_unchanged_to_be_same
-    end #case does not change unchanged
+      before { action }
 
-    it "redirects to the widget :show template" do
-      expect(action).to redirect_to(widget)
-    end #case redirect to :show
+        it "updates the widget with changed values" do
+          expect_reload_to_eq_final
+        end #case updates widget
 
-    it "provides notice of update" do
-      action
-      expect(controller.notice).to eq('Widget was successfully updated')
-    end
+        it "does not change unchanged values" do
+          expect_unchanged_to_be_same
+        end #case does not change unchanged
+
+        it "provides notice of update" do
+          expect(controller.notice).to eq('Widget was successfully updated')
+        end
 
   end #with valid attributes
 
@@ -234,19 +235,19 @@ describe 'PATCH update' do
 
       before { invalid_action }
 
-      it "does not change the widget" do
-        for_the_same_widget
-        expect(widget).to eq(widget.reload)
-      end #case does not change
+        it "provides notice of error" do
+          expect(controller.flash[:error]).to eq("Could not update Widget")
+        end
 
-      it "redirects to the :edit template for @widget" do
-        for_the_same_widget
-        expect(response).to render_template("edit", id: widget.id)
-      end #case redirect to :edit
+        before { for_the_same_widget }
 
-      it "provides notice of error" do
-        expect(controller.flash[:error]).to eq("Could not update Widget")
-      end
+          it "does not change the widget" do
+            expect(widget).to eq(widget.reload)
+          end #case does not change
+
+          it "redirects to the :edit template for @widget" do
+            expect(response).to render_template("edit", id: widget.id)
+          end #case redirect to :edit
 
     end #with invalid attributes
   end #PATCH #update
