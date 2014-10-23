bbd3623b78aa30e026d75a2a451278f95ffa4bed
SRozick <steven.rozick@teachafish.org>
2014-10-22 23:43:02 -0400

    WIP working on menu

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,28 +1,11 @@
-$(function(){
-  $(document).foundation();
-});
-
-// expected to stop scrolling when offcanvas element is open
-// $(document)
-// .on('open.fndtn.offcanvas', '[data-offcanvas]', function() {
-//   $('html').css('overflow', 'hidden');
-// })
-// .on('close.fndtn.offcanvas', '[data-offcanvas]', function() {
-//   $('html').css('overflow', 'auto');
-// })
-
-// Other functions associated to offcanvas menu behaviour
-$(document).foundation({
-  offcanvas : {
-    // Sets method in which offcanvas opens.
-    // [ move | overlap_single | overlap ]
-    // move is default, shifts main content away from menu content
-    // overlap causes menu content to overlap main content
-    // overlap_single causes menu content to overlap single column of main content?
-    open_method: 'move',
-    // Should the menu close when a menu link is clicked?
-    // [ true | false ]
-    // default is false
-    close_on_click : true
-  }
-});
+$(function()
+  {
+    $(document).foundation(
+      {
+        offcanvas :
+        { // see http://foundation.zurb.com/docs/components/offcanvas.html
+          open_method: 'move',      //offcanvas moves content on open
+          close_on_click : true     //click on offcanvas closes menu
+        }
+      });
+  });
